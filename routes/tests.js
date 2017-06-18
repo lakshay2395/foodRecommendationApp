ed94b4f848cbfd2b26ceb5998028fe1db7a5d206
lakshay2395 <bhambri.lakshay.lakshay@gmail.com>
2017-06-18 22:20:23 +0530

    test email service and app.json updated.

--- routes/tests.js
+++ routes/tests.js
@@ -5,6 +5,7 @@ var mailHelper = require("../helpers/mail-helper");
 
 router.get("/sendRegistrationSuccessMail/name/:name/email/:email",function(req,res,next){
     mailHelper.sendRegistrationSuccessMail(req.params.name,req.params.email);
+    res.send("Mail Sent");
 });
 
 module.exports = router;
